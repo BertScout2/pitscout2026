15fc754ebcfa6b4ffe023ef26ba8c96b31b47d10
Bert-03 <bertscout2@outlook.com>
2026-02-18 21:22:03 -0500

    Add project files.

---
+++ pitscout2026/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace pitscout2026
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
