7f19be626b4aa42849777e61ac3bf04624dfe8ad
Bert-03 <bertscout2@outlook.com>
2026-02-19 21:24:27 -0500

    variables for buttons

--- pitscout2026/MainPage.xaml.cs
+++ pitscout2026/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿namespace pitscout2026
+﻿using pitscout2026.Models;
+
+namespace pitscout2026
 {
     public partial class MainPage : ContentPage
     {
+        private readonly PitScout pitscout= new();
 
         public MainPage()
         {
---
+++ pitscout2026/Models/PitScout.cs
@@ -0,0 +1,16 @@
+﻿namespace pitscout2026.Models
+{
+    public class PitScout
+    {
+        public int Drive_Train { get; set; } = 0;
+        public int Preferred_Placement { get; set; } = 0;
+        public bool Climb_Auto { get; set; } = false;
+        public bool Shoot_Auto { get; set; } = false;
+        public string Best_Auto { get; set; } = string.Empty;
+        public int Max_Fuel { get; set; } = 0;
+        public bool Can_Climb { get; set; } = false;
+        public string Strength { get; set; } = string.Empty;
+        public int Fps { get; set; } = 0;
+        public int Travel_Route { get; set; } = 0;
+    }
+}
