aade5e48df539b8677cfe65b50971fb5bc8ec704
Bert-03 <bertscout2@outlook.com>
2026-02-22 15:36:20 -0500

    all buttons and text boxes functional

--- pitscout2026/MainPage.xaml.cs
+++ pitscout2026/MainPage.xaml.cs
@@ -11,85 +11,97 @@ namespace pitscout2026
             InitializeComponent();
         }
 
+        private void Team_Num_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(Team_Num.Text, out int result))
+            {
+                if (pitscout.Team_Num != result)
+                {
+                    pitscout.Team_Num = result;
+                    Team_Num.Text = pitscout.Team_Num.ToString();
+                }
+            }
+            else
+            {
+                Team_Num.Text = pitscout.Team_Num.ToString();
+            }
+        }
         private void Drive_Train_Swerve_Clicked(object? sender, EventArgs e)
         {
             Set_Drive_Train(1);
         }
-        
+
 
         private void Drive_Train_Tank_Clicked(object? sender, EventArgs e)
         {
             Set_Drive_Train(2);
         }
-
-        private void Drive_Train_Other_Clicked(object? sender, EventArgs e)
+        private void Drive_Train_Write_TextChanged(object? sender, TextChangedEventArgs e)
         {
-            Set_Drive_Train(3);
+            Set_Drive_Train(0);
         }
 
         private void Preferred_Placement_Left_Clicked(object? sender, EventArgs e)
         {
-            Set_Preferred_Placement(1);
+            pitscout.Preferred_Placement_Left = !pitscout.Preferred_Placement_Left;
+            Set_Preferred_Placement_Left(pitscout.Preferred_Placement_Left);
         }
 
         private void Preferred_Placement_Middle_Clicked(object? sender, EventArgs e)
         {
-            Set_Preferred_Placement(2);
+            pitscout.Preferred_Placement_Middle = !pitscout.Preferred_Placement_Middle;
+            Set_Preferred_Placement_Middle(pitscout.Preferred_Placement_Middle);
         }
 
         private void Preferred_Placement_Right_Clicked(object? sender, EventArgs e)
         {
-            Set_Preferred_Placement(3);
-        }
-
-        private void Preferred_Placement_None_Clicked(object? sender, EventArgs e)
-        {
-            Set_Preferred_Placement(0);
+            pitscout.Preferred_Placement_Right = !pitscout.Preferred_Placement_Right;
+            Set_Preferred_Placement_Right(pitscout.Preferred_Placement_Right);
         }
 
         private void Auto_Climb_Yes_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Auto_Climb = true;
+            Set_Auto_Climb(true);
         }
 
         private void Auto_Climb_No_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Auto_Climb = false;
+            Set_Auto_Climb(false);
         }
 
         private void Auto_Shoot_Yes_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Auto_Shoot = true;
+            Set_Auto_Shoot(true);
         }
 
         private void Auto_Shoot_No_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Auto_Shoot = false;
+            Set_Auto_Shoot(false);
         }
 
         private void Can_Climb_Yes_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Can_Climb = true;
+            Set_Can_Climb(true);
         }
 
         private void Can_Climb_No_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Can_Climb = false;
+            Set_Can_Climb(false);
         }
 
         private void Travel_Route_Over_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Travel_Route = 1;
+            Set_Travel_Route(1);
         }
 
         private void Travel_Route_Under_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Travel_Route = 2;
+            Set_Travel_Route(2);
         }
 
         private void Travel_Route_Both_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Travel_Route = 3;
+            Set_Travel_Route(3);
         }
 
         private void Best_Auto_TextChanged(object? sender, TextChangedEventArgs e)
@@ -133,42 +145,42 @@ namespace pitscout2026
 
         private void Climb_Level_1_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Climb_Level = 1;
+            Set_Climb_Level(1);
         }
 
         private void Climb_Level_2_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Climb_Level = 2;
+            Set_Climb_Level(2);
         }
 
         private void Climb_Level_3_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Climb_Level = 3;
+            Set_Climb_Level(3);
         }
 
         private void Climb_Level_None_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Climb_Level = 0;
-        }
-
-        private void Climb_Loc_None_Clicked(object? sender, EventArgs e)
-        {
-            pitscout.Climb_Loc = 0;
+            Set_Climb_Level(0);
         }
 
         private void Climb_Loc_Middle_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Climb_Loc = 1;
+            Set_Climb_Loc(1);
         }
 
         private void Climb_Loc_Side_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Climb_Loc = 2;
+            Set_Climb_Loc(2);
         }
 
         private void Climb_Loc_Both_Clicked(object? sender, EventArgs e)
         {
-            pitscout.Climb_Loc = 3;
+            Set_Climb_Loc(3);
+        }
+
+        private void Climb_Loc_None_Clicked(object? sender, EventArgs e)
+        {
+            Set_Climb_Loc(0);
         }
 
         #region setRoutines
@@ -177,17 +189,69 @@ namespace pitscout2026
             pitscout.Drive_Train = value;
             Drive_Train_Swerve.BackgroundColor = value == 1 ? Colors.Green : Colors.Gray;
             Drive_Train_Tank.BackgroundColor = value == 2 ? Colors.Green : Colors.Gray;
-            Drive_Train_Other.BackgroundColor = value == 3 ? Colors.Green : Colors.Gray;
         }
-        private void Set_Preferred_Placement(int value)
+        private void Set_Preferred_Placement_Left(bool value)
         {
-            pitscout.Preferred_Placement = value;
-            Preferred_Placement_Left.BackgroundColor = value == 1 ? Colors.Green : Colors.Gray;
-            Preferred_Placement_Middle.BackgroundColor = value == 2 ? Colors.Green : Colors.Gray;
-            Preferred_Placement_Right.BackgroundColor = value == 3 ? Colors.Green : Colors.Gray;
-            Preferred_Placement_None.BackgroundColor = value == 0 ? Colors.Green : Colors.Gray;
+            pitscout.Preferred_Placement_Left = value;
+            Preferred_Placement_Left.BackgroundColor = value ? Colors.Green : Colors.Gray;
         }
+        private void Set_Preferred_Placement_Middle(bool value)
+        {
+            pitscout.Preferred_Placement_Middle = value;
+            Preferred_Placement_Middle.BackgroundColor = value ? Colors.Green : Colors.Gray;
+        }
+        private void Set_Preferred_Placement_Right(bool value)
+        {
+            pitscout.Preferred_Placement_Right = value;
+            Preferred_Placement_Right.BackgroundColor = value ? Colors.Green : Colors.Gray;
+        }
+        private void Set_Auto_Climb(bool value)
+        {
+            pitscout.Auto_Climb = value;
+            Auto_Climb_Yes.BackgroundColor = value ? Colors.Green : Colors.Gray;
+            Auto_Climb_No.BackgroundColor = !value ? Colors.Green : Colors.Gray;
+        }
+        private void Set_Auto_Shoot(bool value)
+        {
+            pitscout.Auto_Shoot = value;
+            Auto_Shoot_Yes.BackgroundColor = value ? Colors.Green : Colors.Gray;
+            Auto_Shoot_No.BackgroundColor = !value ? Colors.Green : Colors.Gray;
+        }
+        private void Set_Travel_Route(int value)
+        {
+            pitscout.Travel_Route = value;
+            Travel_Route_Over.BackgroundColor = value == 1 ? Colors.Green : Colors.Gray;
+            Travel_Route_Under.BackgroundColor = value == 2 ? Colors.Green : Colors.Gray;
+            Travel_Route_Both.BackgroundColor = value == 3 ? Colors.Green : Colors.Gray;
+        }
+        private void Set_Can_Climb(bool value)
+        {
+            pitscout.Can_Climb = value;
+            Can_Climb_Yes.BackgroundColor = value ? Colors.Green : Colors.Gray;
+            Can_Climb_No.BackgroundColor = !value ? Colors.Green : Colors.Gray;
+        }
+        private void Set_Climb_Level(int value)
+        {
+            pitscout.Climb_Level = value;
+            Climb_Level_1.BackgroundColor = value == 1 ? Colors.Green : Colors.Gray;
+            Climb_Level_2.BackgroundColor = value == 2 ? Colors.Green : Colors.Gray;
+            Climb_Level_3.BackgroundColor = value == 3 ? Colors.Green : Colors.Gray;
+            Climb_Level_None.BackgroundColor = value == 0 ? Colors.Green : Colors.Gray;
+        }
+        private void Set_Climb_Loc(int value)
+        {
+            pitscout.Climb_Loc = value;
+            Climb_Loc_Middle.BackgroundColor = value == 1 ? Colors.Green : Colors.Gray;
+            Climb_Loc_Side.BackgroundColor = value == 2 ? Colors.Green : Colors.Gray;
+            Climb_Loc_Both.BackgroundColor = value == 3 ? Colors.Green : Colors.Gray;
+            Climb_Loc_None.BackgroundColor = value == 0 ? Colors.Green : Colors.Gray;
+        }
+
         #endregion
 
+        private void Comments_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            pitscout.Comments = Comments.Text;
+        }
     }
 }
--- pitscout2026/Models/PitScout.cs
+++ pitscout2026/Models/PitScout.cs
@@ -2,8 +2,11 @@
 {
     public class PitScout
     {
+        public int Team_Num { get; set; } = 0;
         public int Drive_Train { get; set; } = 0;
-        public int Preferred_Placement { get; set; } = 0;
+        public bool Preferred_Placement_Left { get; set; } = false;
+        public bool Preferred_Placement_Middle { get; set; } = false;
+        public bool Preferred_Placement_Right { get; set; } = false;
         public bool Auto_Climb { get; set; } = false;
         public bool Auto_Shoot { get; set; } = false;
         public string Auto_Best { get; set; } = string.Empty;
@@ -14,5 +17,6 @@
         public string Strength { get; set; } = string.Empty;
         public int Fps { get; set; } = 0;
         public int Travel_Route { get; set; } = 0;
+        public string Comments { get; set; } = string.Empty;
     }
 }
