d02ddeada070088e28dba696648354c9739104b1
Bert-03 <bertscout2@outlook.com>
2026-03-01 14:01:06 -0500

    moved buttons away from text

---
+++ pitscout2026/Models/PitScoutbackup.cs
@@ -0,0 +1,46 @@
+﻿namespace pitscout2026.Models
+{
+    public class PitScout
+    {
+        public int Team_Num { get; set; } = 0;
+        public int Drive_Train { get; set; } = 0;
+        public bool Preferred_Placement_Left { get; set; } = false;
+        public bool Preferred_Placement_Middle { get; set; } = false;
+        public bool Preferred_Placement_Right { get; set; } = false;
+        public bool Auto_Climb { get; set; } = false;
+        public bool Auto_Shoot { get; set; } = false;
+        public string Auto_Best { get; set; } = string.Empty;
+        public int Max_Fuel { get; set; } = 0;
+        public bool Can_Climb { get; set; } = false;
+        public int Climb_Level { get; set; } = 0;
+        public int Climb_Loc { get; set; } = 0;
+        public string Strength { get; set; } = string.Empty;
+        public int Fps { get; set; } = 0;
+        public int Travel_Route { get; set; } = 0;
+        public string Comments { get; set; } = string.Empty;
+        public static string CreateTableCommand()
+        {
+            return
+                @"CREATE TABLE pitScout(
+                Id    INTEGER,
+                Team_Num  INTEGER,
+                Drive_Train INTEGER,
+                Preferred_Placement_Left INTEGER,
+                Preferred_Placement_Middle INTEGER,
+                Preferred_Placement_Right INTEGER,
+                Auto_Climb INTEGER,
+                Auto_Shoot INTEGER,
+                Auto_Best TEXT,
+                Max_Fuel INTEGER,
+                Can_Climb INTEGER,
+                Climb_Level INTEGER,
+                Climb_Loc INTEGER,
+                Strength TEXT,
+                Fps INTEGER,
+                Travel_Route INTEGER,
+                Comments TEXT,
+                PRIMARY KEY(Id AUTOINCREMENT)
+                );";
+        }
+    }
+}
