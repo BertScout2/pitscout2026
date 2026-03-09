415804debb8483b107bf314bf27bc0631082a54a
Scott Bakker <sbakker343@gmail.com>
2026-03-08 20:50:23 -0400

    about and airtable

--- pitscout2026/AirtablePage.xaml.cs
+++ pitscout2026/AirtablePage.xaml.cs
@@ -2,8 +2,8 @@ namespace pitscout2026;
 
 public partial class AirtablePage : ContentPage
 {
-	public AirtablePage()
-	{
-		InitializeComponent();
-	}
+    public AirtablePage()
+    {
+        InitializeComponent();
+    }
 }
