ec02bfc6e0b4d7120c3c7ef7627dc1b79fffe361
Scott Bakker <sbakker343@gmail.com>
2026-03-09 18:16:26 -0400

    addsingleton

--- pitscout2026/AirtablePage.xaml.cs
+++ pitscout2026/AirtablePage.xaml.cs
@@ -1,9 +1,16 @@
+using pitscout2026.Database;
+
 namespace pitscout2026;
 
 public partial class AirtablePage : ContentPage
 {
-    public AirtablePage()
+    private readonly PitDataBase db = new();
+    private readonly GlobalViewModel _global;
+
+    public AirtablePage(GlobalViewModel global)
     {
         InitializeComponent();
+        _global = global;
+        BindingContext = _global;
     }
 }
--- pitscout2026/MauiProgram.cs
+++ pitscout2026/MauiProgram.cs
@@ -19,6 +19,8 @@ namespace pitscout2026
     		builder.Logging.AddDebug();
 #endif
 
+            builder.Services.AddSingleton<GlobalViewModel>(new GlobalViewModel());
+            
             return builder.Build();
         }
     }
